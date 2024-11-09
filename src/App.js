fa903cf2aa6f87127804dd3789316dd9b2c9ad76
Ben Dyson <benj.dyson@gmail.com>
2024-11-09 17:38:57 +0000

    Sorted routing and added a navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, BrowserRouter as Router, Link} from "react-router-dom";
+import Home from './routes/Home.js';
+import Profile from './routes/Profile.js';
+import Login from './routes/Login.js';
+import Register from './routes/Register.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/Home" exact Component={Home} />
+          <Route path="/Index" exact Component={Home} />
+          <Route path="/profile" Component={Profile} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+// import { useState, useEffect } from "react";
+// import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+// import '../App.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const linkStyle = {
+    margin: "1rem",
+    textDecoration: "none",
+    color: 'Black'
+  };
+
+export const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <div className="navcontainer">
+                <Link to='/' style={linkStyle}>Home</Link>
+                <Link to='profile' style={linkStyle}>Profile</Link>
+                </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
