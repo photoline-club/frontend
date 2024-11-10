64b97cbb48b706a7d27d53bde0c672a8bc0455f5
Patrick Jones <git@patrickjones.uk>
2024-11-10 05:13:43 +0000

    centre forms

--- src/routes/Add_Event.js
+++ src/routes/Add_Event.js
@@ -1,52 +1,57 @@
 import React, { useState, useEffect } from "react";
-import '../App.css';
+import "../App.css";
 import { NavBar } from "../components/NavBar";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
-import { Button } from 'primereact/button';
-import { Calendar } from 'primereact/calendar';
-import { MultiSelect } from 'primereact/multiselect';
-import { useNavigate } from 'react-router-dom';
+import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
+import { MultiSelect } from "primereact/multiselect";
+import { useNavigate } from "react-router-dom";
 import { api } from "../const.js";
 
 const getData = async () => {
-    const response = await fetch(api+'/api/friends', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+localStorage.getItem("CurrentUser")
-        }
-    });
-    if (response.status === 401) {
-        throw (new Error());
-    } else if (response.status !== 200) {
-        alert("Error")
-        return;
-    }
+  const response = await fetch(api + "/api/friends", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("CurrentUser"),
+    },
+  });
+  if (response.status === 401) {
+    throw new Error();
+  } else if (response.status !== 200) {
+    alert("Error");
+    return;
+  }
 
-    const json = await response.json();
+  const json = await response.json();
 
-    return json.data;
-
-}
+  return json.data;
+};
 
 const fetchData = async (title, description, start_date, end_date, users) => {
-    const response = await fetch(api+'/api/events', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+localStorage.getItem("CurrentUser")
-        },
-        body: JSON.stringify({ "title": title, "description": description, "start_date": start_date, "end_date": end_date, "users": users})
-    });
+  const response = await fetch(api + "/api/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("CurrentUser"),
+    },
+    body: JSON.stringify({
+      title: title,
+      description: description,
+      start_date: start_date,
+      end_date: end_date,
+      users: users,
+    }),
+  });
 
-    if (response.status === 401) {
-        throw (new Error());
-    } else if (response.status !== 201) {
-        alert("Error");
-        return;
-    }
-}
+  if (response.status === 401) {
+    throw new Error();
+  } else if (response.status !== 201) {
+    alert("Error");
+    return;
+  }
+};
 
 // const handleSubmit = () => {
 //     fetchData(name, description, start_date, end_date, friends);
@@ -54,61 +59,109 @@ const fetchData = async (title, description, start_date, end_date, users) => {
 // }
 
 export default function Add_Event() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [start_date, setStartDate] = useState('');
-    const [end_date, setEndDate] = useState('');
-    const [selected, setSelected] = useState('');
-    const [to_display, setDisplay] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [start_date, setStartDate] = useState("");
+  const [end_date, setEndDate] = useState("");
+  const [selected, setSelected] = useState("");
+  const [to_display, setDisplay] = useState([]);
 
-    useEffect(() => {
-        getData().then((friendsList)=>{
-            setDisplay([]);
-            friendsList.forEach(f => setDisplay([...to_display, {name: f.friend.username, id:f.friend.id}]));
-        }).catch(()=>{navigate('/login');})
-    }, [navigate]);
+  useEffect(() => {
+    getData()
+      .then((friendsList) => {
+        setDisplay([]);
+        friendsList.forEach((f) =>
+          setDisplay([
+            ...to_display,
+            { name: f.friend.username, id: f.friend.id },
+          ])
+        );
+      })
+      .catch(() => {
+        navigate("/login");
+      });
+  }, [navigate]);
 
-    const handleSubmit = () => {
-        fetchData(name, description, start_date, end_date, selected).catch(()=>{navigate('/login');});
-        setName(''); setDescription(''); setStartDate(''); setEndDate(''); setSelected(''); setDisplay([]);
-    }
+  const handleSubmit = () => {
+    fetchData(name, description, start_date, end_date, selected).catch(() => {
+      navigate("/login");
+    });
+    setName("");
+    setDescription("");
+    setStartDate("");
+    setEndDate("");
+    setSelected("");
+    setDisplay([]);
+  };
 
+  return (
+    <div className="container" style={{ height: "100%" }}>
+      <NavBar />
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: ".5rem",
+          }}
+        >
+          <div>Event title:</div>
+          <InputText value={name} onChange={(e) => setName(e.target.value)} />
+          <div>Description:</div>
+          <div>
+            <InputTextarea
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              rows={5}
+              cols={30}
+            />
+          </div>
 
-    return (
-        <div className="container">
-            <NavBar />
-            <div>
-                <div>Event title:</div>
-                <InputText value={name} onChange={(e) => setName(e.target.value)} />
-                <div>Description:</div>
-                <div>
-                <InputTextarea
-                                id="description"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                                rows={5}
-                                cols={30}
-                            />
-                </div>
-
-                <div>Start Date</div>
-                <div>
-                <Calendar value={start_date} onChange={(e) => setStartDate(e.value)} />
-                </div>
-                <div>End Date</div>
-                <div>
-                <Calendar value={end_date} onChange={(e) => setEndDate(e.value)} />
-                </div>
-                <div>Friends</div>
-                <div >
-                    <MultiSelect value={selected} onChange={(e) => setSelected(e.value)} options={to_display} optionLabel="name" optionValue="id" display="chip"
-                    placeholder="Select Friends" className="w-full md:w-20rem" />
-                </div>
-                <div><Button label="Submit" onClick={handleSubmit} /></div>
-            </div>
+          <div>Start Date</div>
+          <div>
+            <Calendar
+              value={start_date}
+              onChange={(e) => setStartDate(e.value)}
+              dateFormat="dd/mm/yy"
+            />
+          </div>
+          <div>End Date</div>
+          <div>
+            <Calendar
+              value={end_date}
+              onChange={(e) => setEndDate(e.value)}
+              dateFormat="dd/mm/yy"
+            />
+          </div>
+          <div>Friends</div>
+          <div>
+            <MultiSelect
+              value={selected}
+              onChange={(e) => setSelected(e.value)}
+              options={to_display}
+              optionLabel="name"
+              optionValue="id"
+              display="chip"
+              placeholder="Select Friends"
+              className="w-full md:w-20rem"
+            />
+          </div>
+          <div>
+            <Button label="Submit" onClick={handleSubmit} />
+          </div>
         </div>
-    );
-
+      </div>
+    </div>
+  );
 }
+
--- src/routes/Friend.js
+++ src/routes/Friend.js
@@ -1,124 +1,128 @@
-import React, { useState, useEffect }  from "react";
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import "../App.css";
 import { NavBar } from "../components/NavBar";
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { useNavigate } from 'react-router-dom';
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 import { api } from "../const.js";
 
 const top_container_style = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  height: "100%",
+  display: "flex",
+  rowGap: "1rem",
+  justifyContent: "center",
+  alignItems: "center",
 };
 
 const middle_container_style = {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    top: '0',
-    bottom: '0',
-    left: '0',
-    right: '0',
-
-    margin: 'auto'
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
 };
 
 const form_label_style = {
-    marginTop: "2vh"
-
+  marginTop: "2vh",
 };
 
 const form_bottom_style = {
-    marginTop: "5vh"
+  marginTop: "5vh",
 };
 
-
 const side_div1 = {
-    display:'inline-block',
-    margin: '1rem',
+  display: "inline-block",
+  margin: "1rem",
 };
 
 const side_div2 = {
-    display:'inline-block',
-    margin: '1rem',
+  display: "inline-block",
+  margin: "1rem",
 };
 
 const getCode = async () => {
-    const response = await fetch(api+'/api/friendcode', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+localStorage.getItem("CurrentUser")
-        }
-    });
+  const response = await fetch(api + "/api/friendcode", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("CurrentUser"),
+    },
+  });
 
-    if (response.status === 401) { //Needs to login
-        throw (new Error());
-    } else if (response.status !== 200) { // Check success code
-        return "not code found";
-    }
+  if (response.status === 401) {
+    //Needs to login
+    throw new Error();
+  } else if (response.status !== 200) {
+    // Check success code
+    return "not code found";
+  }
 
-    const json = await response.json();
+  const json = await response.json();
 
-    return json.code;
-}
+  return json.code;
+};
 
 const fetchData = async (code) => {
-    const response = await fetch(api+'/api/friends', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+localStorage.getItem("CurrentUser")
-        },
-        body: JSON.stringify({ 'token': code})
-    });
+  const response = await fetch(api + "/api/friends", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("CurrentUser"),
+    },
+    body: JSON.stringify({ token: code }),
+  });
 
-    if (response.status === 409) {
-        alert("Already added as a friend");
-        return;
-    } else if (response.status === 401) {
-        throw(new Error());
-    } else if (response.status === 404) {
-        alert('invalid code');
-        return;
-    }
-    if (response.status !==201) {
-        alert("Error");
-        return;
-    }
-}
+  if (response.status === 409) {
+    alert("Already added as a friend");
+    return;
+  } else if (response.status === 401) {
+    throw new Error();
+  } else if (response.status === 404) {
+    alert("invalid code");
+    return;
+  }
+  if (response.status !== 201) {
+    alert("Error");
+    return;
+  }
+};
 
 export default function Friend() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [code, setCode] = useState('');
-    useEffect(() => {
-        getCode().then(setCode).catch(()=>{navigate('/login');})
-    }, [navigate]);
+  const [code, setCode] = useState("");
+  useEffect(() => {
+    getCode()
+      .then(setCode)
+      .catch(() => {
+        navigate("/login");
+      });
+  }, [navigate]);
 
-    const [codeEntered, setCodeEntered] = useState('');
+  const [codeEntered, setCodeEntered] = useState("");
 
-    const handleSubmit = () => {
-        fetchData(codeEntered).catch(()=>{navigate('/login');});
-        setCodeEntered('');
-    }
+  const handleSubmit = () => {
+    fetchData(codeEntered).catch(() => {
+      navigate("/login");
+    });
+    setCodeEntered("");
+  };
 
-    return (
-        <div>
-            <NavBar />
-            <div style={top_container_style}>
-                <div style={middle_container_style}>
-                <div>
-                    Your code: { code }
-                </div>
-                <div>
-                    Enter code:
-                </div>
-                <InputText value={codeEntered} onChange={(e) => setCodeEntered(e.target.value)} />
-                <div><Button label="Submit" onClick={handleSubmit} /></div>
-                </div>
-            </div>
+  return (
+    <div style={{ height: "100%" }}>
+      <NavBar />
+      <div style={top_container_style}>
+        <div style={middle_container_style}>
+          <div style={{fontSize: 'xx-large', color: 'black'}}>Your code: {code}</div>
+          <div>Enter code:</div>
+          <InputText
+            value={codeEntered}
+            onChange={(e) => setCodeEntered(e.target.value)}
+          />
+          <div>
+            <Button label="Submit" onClick={handleSubmit} />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
