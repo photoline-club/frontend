1dbd2a3449e16171eae1e79979c843ad4e38517b
Patrick Jones <git@patrickjones.uk>
2024-11-10 06:08:36 +0000

    responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,70 @@
-// import { useState, useEffect } from "react";
-// import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import '../App.css';
-import logo from '../logo (1).png';
+import logo from "../logo (1).png";
 
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const navbar_logo = {
-    //marginTop: "1rem",
-    marginLeft: "1rem",
+  marginLeft: "1rem",
 };
 
 const navbar_main = {
-    marginTop: "1.5rem",
-    marginLeft: "1.5rem",
+  marginTop: "1.5rem",
+  marginLeft: "1.5rem",
 };
 
 const linkStyle = {
-    margin: "1rem",
-    textDecoration: "none",
-    color: 'Black'
+  margin: "1rem",
+  textDecoration: "none",
+  color: "Black",
 };
 
 export const NavBar = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <div style = {{ display:'flex', justifyContent:"space-between", alignItems:"center", width:"100%", marginTop: '1rem' }}>
-            <div style={ navbar_logo }>
-                <img src={ logo } alt='Logo'/>
-                </div>
-                <div className="" style={{ marginTop: '0vh' }}>
-                    <Link to='/home' style={linkStyle}>Home</Link>
-                    { ("CurrentUser" in localStorage)?
-                    <Link to='/logout' style={linkStyle}>Logout</Link>
-                    :<Link to='/login' style={linkStyle}>Login</Link>}
-                    { ("CurrentUser" in localStorage)?
-                    <Link to='/friend' style={linkStyle}>Add Friend</Link>:''}
-                    { ("CurrentUser" in localStorage)?
-                    <Link to='/add_event' style={linkStyle}>Add Event</Link>:''}
-            </div>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+          marginTop: "1rem",
+        }}
+      >
+        <div style={navbar_logo}>
+          <img src={logo} alt="Logo" />
         </div>
-        </Navbar>
-    );
-}
+        <div className="" style={{ display: "flex", flexWrap: "wrap" }}>
+          <Link to="/home" style={linkStyle}>
+            Home
+          </Link>
+          {"CurrentUser" in localStorage ? (
+            <Link to="/logout" style={linkStyle}>
+              Logout
+            </Link>
+          ) : (
+            <Link to="/login" style={linkStyle}>
+              Login
+            </Link>
+          )}
+          {"CurrentUser" in localStorage ? (
+            <Link to="/friend" style={linkStyle}>
+              Add Friend
+            </Link>
+          ) : (
+            ""
+          )}
+          {"CurrentUser" in localStorage ? (
+            <Link to="/add_event" style={linkStyle}>
+              Add Event
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </Navbar>
+  );
+};
+
