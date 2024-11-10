07b0eca16672a71f51241304639872386e91d2d6
Patrick Jones <git@patrickjones.uk>
2024-11-10 05:57:04 +0000

    improve gallery layout

--- src/routes/Images.js
+++ src/routes/Images.js
@@ -1,12 +1,12 @@
 import { Image } from "primereact/image"; // Import the Image component from PrimeReact
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom"; // Import useParams from react-router-dom
 import { useEffect, useState } from "react";
 import { api } from "../const";
 import { NavBar } from "../components/NavBar";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Button } from "primereact/button";
 const ImageGallery = () => {
-
   const navigate = useNavigate();
   let { id } = useParams();
   const [files, setFiles] = useState([]);
@@ -24,7 +24,6 @@ const ImageGallery = () => {
           return;
         }
         if (response.status !== 200) {
-
           navigate(`/events/${id}/upload`);
           return;
         }
@@ -45,25 +44,36 @@ const ImageGallery = () => {
   return (
     <div className="Root">
       <NavBar></NavBar>
-      <div className="image-gallery">
-
-        {files.map((file, index) => (
-          <div key={index}>
-            <Image
-              key={index}
-              src={`${api}/images/${file.filename}.${file.type}`}
-              alt={`Image ${index + 1}`}
-              width="250"
-              preview
-            />
-            <div className="username-posted-image">
-              <small>{file.user}</small>
+      <div
+        className="image-gallery"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "1rem",
+          marginTop: "5rem",
+        }}
+      >
+        <Link to={`/events/${id}/upload`}>
+          <Button label="Upload" />
+        </Link>
+        <div style={{display: 'flex', gap: '2rem', flexWrap: 'wrap', justifyContent: 'center'}}>
+          {files.map((file, index) => (
+            <div key={index}>
+              <Image
+                key={index}
+                src={`${api}/images/${file.filename}.${file.type}`}
+                alt={`Image ${index + 1}`}
+                width="250"
+                preview
+              />
+              <div className="username-posted-image">
+                <span style={{ color: "black" }}>Uploaded by {file.user}</span>
+              </div>
             </div>
-
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <Link to={`/events/${id}/upload`}>upload</Link>
     </div>
   );
 };
