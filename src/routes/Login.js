f61e89349483db4c3cf5aff3cb94bb870b9dbf34
Patrick Jones <git@patrickjones.uk>
2024-11-10 07:05:40 +0000

    fix login form

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Password } from 'primereact/password';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
+import { Password } from "primereact/password";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 import { api } from "../const.js";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const top_container_style = {
   display: "flex",
@@ -18,35 +18,31 @@ const middle_container_style = {
 };
 
 const form_label_style = {
-  marginTop: "2vh"
-
+  marginTop: "2vh",
 };
 
-const form_bottom_style = {
-  marginTop: "5vh"
-};
-
+const form_bottom_style = {};
 
 const side_div1 = {
-  display: 'inline-block',
-  margin: '1rem',
+  marginTop: "1rem",
+  display: "inline-block",
 };
 
 const side_div2 = {
-  display: 'flex',
-  margin: '1rem',
+  marginTop: "1rem",
+  display: "flex",
 };
 
 const fetchData = async (name, pass) => {
   try {
-    const response = await fetch(api + '/api/login', {
-      method: 'POST',
-      headers: { 'authorization': name + ':' + pass, },
-      body: JSON.stringify({ username: name, password: pass })
+    const response = await fetch(api + "/api/login", {
+      method: "POST",
+      headers: { authorization: name + ":" + pass },
+      body: JSON.stringify({ username: name, password: pass }),
     });
 
     if (response.status !== 201) {
-      throw (new Error());
+      throw new Error();
     }
     const json = await response.json();
     localStorage.setItem("CurrentUser", json.token);
@@ -56,22 +52,23 @@ const fetchData = async (name, pass) => {
   } catch (error) {
     alert("Username or password is wrong");
   }
-}
+};
 
 export default function Login() {
-  const [pass, setPass] = useState('');
-  const [name, setName] = useState('');
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
 
   const navigate = useNavigate();
 
   const handleSubmit = () => {
-    fetchData(name, pass).then(() => { navigate('/home'); });
-  }
+    fetchData(name, pass).then(() => {
+      navigate("/home");
+    });
+  };
 
   const handleRegisterSubmit = () => {
-    navigate('/register');
-  }
-
+    navigate("/register");
+  };
 
   return (
     <div style={top_container_style}>
@@ -80,9 +77,19 @@ export default function Login() {
         <div style={form_label_style}>Username:</div>
         <InputText value={name} onChange={(e) => setName(e.target.value)} />
         <div style={form_label_style}>Password:</div>
-        <Password value={pass} onChange={(e) => setPass(e.target.value)} feedback={false} tabIndex={0} />
-        <div style={form_bottom_style}><div style={side_div1}><Button label="Submit" onClick={handleSubmit} /></div>
-          <div style={side_div2}><Button label="Register" onClick={handleRegisterSubmit} /></div>
+        <Password
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          feedback={false}
+          tabIndex={0}
+        />
+        <div style={form_bottom_style}>
+          <div style={side_div1}>
+            <Button label="Submit" onClick={handleSubmit} />
+          </div>
+          <div style={side_div2}>
+            <Button label="Register" onClick={handleRegisterSubmit} />
+          </div>
         </div>
       </div>
     </div>
